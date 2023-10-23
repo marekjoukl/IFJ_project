1391fc5348ba6622ca380bb6929db9976d05b534
hepaku <jakub.zelenay@gmail.com>
2023-10-23 18:41:55 +0200

    added automaton states and transition function

---
+++ scanner.h
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+typedef enum AutomatState
+{
+    Error,
+    Start,
+    Ls,
+    LsEqual,
+    Gt,
+    GtEqual,
+    ExlPoint,
+    NotEqual,
+    Assign,
+    Equal,
+    Comma,
+    Colon,
+    RPar,
+    LPar,
+    SingleQmark,
+    DoubleQmark,
+    Id,
+    IdTypeNil,
+    BeginString,
+    StringLit,
+    EscSeq,
+    EscU,
+    EscLBr,
+    FirstHex,
+    SecondHex,
+    EndStringLit,
+    EmptyString,
+    MltLnStringLit,
+    FirstQuote,
+    SecondQuote,
+    EndMltLnStringLit,
+    IntLit,
+    DecPoint,
+    DoubleLit,
+    Exp,
+    ExpSign,
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    Comment,
+    CommentBody,
+    NestedComment,
+    CommentEnding,
+    BlockComment,
+    Space,
+    NewLine,
+} AutomatState;
+
+AutomatState transition(AutomatState current, char edge);
