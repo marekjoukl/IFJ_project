30de890d093212f246635a0887fa6b17aceb76e8
Ondrej Kozanyi <ondrej.kozanyi@gmail.com>
2023-11-21 20:20:57 +0100

    updated parser.c with rule no.3 and declared needed functions in parser.h

--- src/parser.c
+++ src/parser.c
@@ -4,6 +4,148 @@
  * @brief Parser using recursive descent
  */
 
-#include "parser.c"
+#include "parser.h"
 
+bool Prog(Lexeme token) {
+    if (token.kind == EOF) return true;
+    if (token.kind == IDENTIFIER || token.kind == FUNC || token.kind == IF || token.kind == LET || token.kind == WHILE || token.kind == VAR) {
+        Sequence(token);
+        return true;
+    }
+    return false;
+}
 
+bool Sequence(Lexeme token) {
+    if (token.kind == RIGHT_BRACKET || token.kind == EOF) return true;
+    if (token.kind == LET || token.kind == VAR) {
+        if (!VarDef(token))
+            exit(2);
+        token = get_next_non_whitespace_lexeme();
+
+        if (token.kind != IDENTIFIER)
+            exit(2);
+
+        token = get_next_non_whitespace_lexeme();
+
+        if (!VarTypeDef(token))
+            exit(2);
+
+        token = get_next_non_whitespace_lexeme();
+
+        if (!AssignVar(token))
+            exit(2);
+
+        token = get_next_non_whitespace_lexeme();
+
+        Sequence(token);
+        return true;
+    }
+
+    //TODO: dokoncit
+    else if (token.kind == IDENTIFIER) {
+        if (!AssignOrFunction(token))
+            exit(2);
+        return true;
+    }
+    else if (token.kind == IF) {
+        if (!IfExp(token))
+            exit(2);
+        return true;
+    }
+    else if (token.kind == WHILE) {
+        return true;
+    }
+    else if (token.kind == FUNC) {
+        if (!DefFunction(token))
+            exit(2);
+        return true;
+    }
+
+    //ukoncenie rekurzie
+    else if (token.kind == RIGHT_BRACKET || token.kind == LEX_EOF) {
+        return true;
+    }
+}
+
+bool VarDef(Lexeme token) {
+    if (token.kind == LET || token.kind == VAR) {
+        return true;
+    }
+    return false;
+}
+
+bool VarTypeDef(Lexeme token) {
+    if (token.kind == COLON) {
+        token = get_next_non_whitespace_lexeme();
+        if (!Type(token))
+            exit(2);
+        return true;
+    }
+
+    else if (token.kind == ASSIGNMENT || token.kind == LEX_EOF || token.kind == RIGHT_BRACKET) {
+        return true;
+    }
+
+    return false;
+}
+
+bool Type(Lexeme token) {
+    if (token.kind == INT) {
+        token = get_next_non_whitespace_lexeme();
+        if(!QMark(token))
+            exit(2);
+        return true;
+    }
+    else if (token.kind == STRING) {
+        token = get_next_non_whitespace_lexeme();
+        if(!QMark(token))
+            exit(2);
+        return true;
+    }
+    else if (token.kind == DOUBLE) {
+        token = get_next_non_whitespace_lexeme();
+        if(!QMark(token))
+            exit(2);
+        return true;
+    }
+    return false;
+}
+
+bool QMark(Lexeme token) {
+    if (token.kind == QUESTION_MARK) {
+        return true;
+    }
+    else if (token.kind == ASSIGNMENT || token.kind == LEX_EOF || token.kind == RIGHT_BRACKET || token.kind == COMMA || token.kind == LEFT_BRACKET || token.kind == RIGHT_PAR) {
+        return true;
+    }
+    return false;
+}
+
+bool AssignVar(Lexeme token) {
+    if (token.kind == ASSIGNMENT) {
+        token = get_next_non_whitespace_lexeme();
+        if (!ExpOrCall(token))
+            exit(2);
+        return true;
+    }
+    else if (token.kind == IDENTIFIER || token.kind == RIGHT_BRACKET || token.kind == LEX_EOF || token.kind == FUNC || token.kind == IF || token.kind == LET || token.kind == WHILE || token.kind == VAR) {
+        return true;
+    }
+    return false;
+}
+
+bool ExpOrCall(Lexeme token) {
+    if (token.kind == IDENTIFIER) {             //TODO: FIND OUT WHAT RULE TO USE (26. / 27.)
+        token = get_next_non_whitespace_lexeme();
+        if (!CallFunction(token))
+            exit(2);
+        return true;
+    }
+    else if ((token.kind == IDENTIFIER /* TODO: look into symtable if id is var or func */) || token.kind == INTEGER_LIT || token.kind == DOUBLE_LIT || token.kind == STRING_LIT || token.kind == LEFT_PAR) {
+        token = get_next_non_whitespace_lexeme();
+        if (!Expression(token))
+            exit(2);
+        return true;
+    }
+    return false;
+}
--- src/parser.h
+++ src/parser.h
@@ -4,6 +4,43 @@
  * @brief Header for parser
  */
 
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "scanner.h"
+
+bool Prog(Lexeme token);
+bool Sequence(Lexeme token);
+bool VarDef(Lexeme token);
+bool VarTypeDef(Lexeme token);
+bool AssignVar(Lexeme token);
+bool DefFunction(Lexeme token);
+bool FirstParamDef(Lexeme token);
+bool ParamsDef(Lexeme token);
+bool ParamsNameDef(Lexeme token);
+bool ParamsDefN(Lexeme token);
+bool ReturnFunction(Lexeme token);
+bool AssignOrFunction(Lexeme token);
+bool ExpOrCall(Lexeme token);
+bool CallFunction(Lexeme token);
+bool FirstParam(Lexeme token);
+bool ParamsN(Lexeme token);
+bool Params(Lexeme token);
+bool ParamsName(Lexeme token);
+bool ElseStat(Lexeme token);
+bool IfExp(Lexeme token);
+bool Type(Lexeme token);
+bool QMark(Lexeme token);
+bool IdOrLit(Lexeme token);
+
+
+//TODO: find out how to call precendencnu analyzu
+bool Expression(Lexeme token);
+
+#endif
 
 
