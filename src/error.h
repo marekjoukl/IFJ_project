c70e37b281fc64ba1d430e827978a95b5aef9096
hepaku <jakub.zelenay@gmail.com>
2023-11-28 17:01:39 +0100

    Add error.h header file with error code definitions

---
+++ src/error.h
@@ -0,0 +1,15 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#define LEXICAL_ERROR 1             // lexical error
+#define SYNTAX_ERROR 2              // syntax error   
+#define DEFINITION_ERROR 3          // undifined function or redefinition of variable
+#define PARAMETER_TYPE_ERROR 4      // wrong number or type of parameters
+#define UNDIFINED_VAR_ERROR 5       // use of undifined or uninitialized variable
+#define RETURN_ERROR 6              // return statement in void function or no return statement in non-void function
+#define TYPE_ERROR 7                // wrong type of operands in arithmetic, string or relational expression
+#define TYPE_DEDUCTION_ERROR 8      // type of variable or parameter is not specified and cannot be deduced
+#define OTHER_SEMANTIC_ERROR 9      // other semantic errors
+#define INTERNAL_ERROR 99           // internal error - memory allocation, etc.
+
+#endif // ERROR_H
