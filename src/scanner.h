690368e22f3b9a4fd0f321824923db8357522d96
hepaku <jakub.zelenay@gmail.com>
2023-12-04 22:16:42 +0100

    Add get_next_new_line_or_lexeme function to scanner.h

--- src/scanner.h
+++ src/scanner.h
@@ -80,6 +80,7 @@ typedef struct Buffer
 Lexeme make_lexeme(AutomatState current, char *str);
 Lexeme get_lexeme(void);
 Lexeme get_next_non_whitespace_lexeme(void);
+Lexeme get_next_new_line_or_lexeme(void);
 void add_to_string(char **str, size_t *len, size_t *capacity, char edge, AutomatState current, AutomatState next);
 
 #endif // SCANNER_H
